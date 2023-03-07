05706fa0d3a04681e5a3c1af017ebeed75229cf0
kaigroden-gilchrist <kaigg@students.cs.ubc.ca>
2023-03-06 18:01:19 -0800

    reinstalled updated some tests to JUNIT 5

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,7 +1,7 @@
 package persistence;
 
 import model.Portfolio;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-
 public class JsonWriterTest {
 
     @Test
